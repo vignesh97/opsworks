47795fff7b818d5290f72d074fa695402c3e903e
Vigneshwaran Ravimurugan <vigneshr2311@gmail.com>
2016-06-21 20:57:27 +0530

    Chef 12 syntax
    Chef 12 syntax

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -13,7 +13,7 @@ directory data_dir do
   recursive true
   action :create
 end
-
+=begin
 gem_package "aws-sdk" do
   action :install
 end
@@ -32,4 +32,32 @@ ruby_block "download-object" do
     end
   end
   action :run
+end
+=end
+
+chef_gem "aws-sdk" do
+  compile_time false
+end
+
+# Define that aws-sdk is required
+require 'aws-sdk'
+# Declare the s3 object as an S3 client
+s3 = Aws::S3::Client.new(region:'us-east-1')
+
+# Set bucket and object name
+obj = s3.get_object(bucket:'dev.rivet.media.web', key:'test.txt')
+
+# Read content to variable
+content = obj.body.read
+Chef::Log.info(content)
+
+# Log output (optional)
+
+# Write the S3 content to file (in /tmp)
+file '/tmp/test.txt' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  content content
+  action :create
 end
